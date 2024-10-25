e75a8f574d74f002ce802e781d3cb96ab14e341e
Vladislav <666darksite666@mail.ru>
2024-10-25 19:16:59 +0300

    Added Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задание 1: Поиск элемента в массиве
+// Описание: Задайте массив целых чисел. Напишите программу, которая проверяет,
+// присутствует ли заданное число в массиве. Программа должна вывести:
+// Присутствует/Не присутствует.
+
+int[] array = {1, 3, 4, 19, 3};
+int num = 4;
+
+bool isExistNum = false;
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+
+string result = isExistNum ? "Присутствует" : "Не присутствует";
+Console.WriteLine(result);
+
